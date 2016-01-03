9fb67f0803d5b8df01152e5abd138d0663d07002
baixue <baixue@baixues-macbook-pro.local>
2016-01-03 12:28:21 +0800

    Initial Commit

---
+++ 切换视图_练习/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  切换视图_练习
+//
+//  Created by baixue on 16/1/3.
+//  Copyright © 2016年 baixue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 切换视图_练习/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  切换视图_练习
+//
+//  Created by baixue on 16/1/3.
+//  Copyright © 2016年 baixue. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
